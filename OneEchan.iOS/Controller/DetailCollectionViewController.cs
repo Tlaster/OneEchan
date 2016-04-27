f7c42300ff552bcd3f0677910f938e345ab5fead
Tlaster <tlaster@outlook.com>
2016-04-27 23:03:06 +0800

    complete ios version

---
+++ OneEchan.iOS/Controller/DetailCollectionViewController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using CoreGraphics;
+using Foundation;
+using MediaPlayer;
+using OneEchan.Core.Common.Api.Model;
+using OneEchan.Shared;
+using UIKit;
+
+namespace OneEchan.iOS.Controller
+{
+    public partial class DetailCollectionViewController : UICollectionViewController
+    {
+        public List<AnimateSetModel> List { get; private set; } = new List<AnimateSetModel>();
+        public UIRefreshControl RefreshControl { get; set; } = new UIRefreshControl();
+        private const string _detailCellId = "DetailCell";
+        private bool _isLoading;
+
+        public int ID { get; internal set; }
+
+        public DetailCollectionViewController(IntPtr handle) : base(handle)
+        {
+            //RefreshControl = new UIRefreshControl();
+        }
+
+        private async void Refresh()
+        {
+            if (_isLoading) return;
+            _isLoading = true;
+            InvokeOnMainThread(() => RefreshControl.BeginRefreshing());
+            var item = await Core.Common.Api.Detail.GetDetail(ID, LanguageHelper.PrefLang);
+            List = item.SetList;
+            CollectionView.ReloadData();
+            InvokeOnMainThread(() => RefreshControl.EndRefreshing());
+            _isLoading = false;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+        public override nint GetItemsCount(UICollectionView collectionView, nint section) => List.Count;
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            var cell = (DetailCell)CollectionView.DequeueReusableCell(_detailCellId, indexPath);
+            cell.SetImage(List[indexPath.Row].FileThumb,List[indexPath.Row].FileName);
+            return cell;
+        }
+        MPMoviePlayerController moviePlayer;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            RefreshControl.ValueChanged += (sender, e) => Refresh();
+            CollectionView.AddSubview(RefreshControl);
+            var deletate = new CustomFlowLayoutDelegate();
+            deletate.ItemClick += (sender,e) =>
+             {
+                 Core.Common.Api.Detail.AddClick(ID, int.Parse(List[e.Index].FileName), LanguageHelper.PrefLang);
+                 if (moviePlayer == null)
+                 {
+                     moviePlayer = new MPMoviePlayerController();
+                     View.AddSubview(moviePlayer.View);
+                     moviePlayer.ShouldAutoplay = true;
+                 }
+                 moviePlayer.ContentUrl = NSUrl.FromString(List[e.Index].FilePath);
+                 moviePlayer.SetFullscreen(true, false);
+                 moviePlayer.PrepareToPlay();
+                 moviePlayer.Play();
+             };
+            CollectionView.Delegate = deletate;
+            CollectionView.RegisterClassForCell(typeof(DetailCell), _detailCellId);
+            CollectionView.ContentInset = new UIEdgeInsets(4, 4, 4, 4);
+            Refresh();
+        }
+    }
+    public class ItemClickEventArgs : EventArgs
+    {
+        public int Index { get; }
+        public ItemClickEventArgs(int index)
+        {
+            Index = index;
+        }
+    }
+    public class CustomFlowLayoutDelegate : UICollectionViewDelegateFlowLayout
+    {
+        public event EventHandler<ItemClickEventArgs> ItemClick;
+        public override void ItemSelected(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            ItemClick?.Invoke(this, new ItemClickEventArgs(indexPath.Row));
+        }
+        public override CGSize GetSizeForItem(UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+        {
+            var colCount = 2f;
+            if (UIDevice.CurrentDevice.Model.ToLower().Contains("ipad")) colCount = 3f;
+            var width = UIScreen.MainScreen.Bounds.Width / colCount - 8;
+            return new CGSize(width, width / 16f * 9f);
+        }
+        public override nfloat GetMinimumInteritemSpacingForSection(UICollectionView collectionView, UICollectionViewLayout layout, nint section)
+        {
+            return 4f;
+        }
+        public override nfloat GetMinimumLineSpacingForSection(UICollectionView collectionView, UICollectionViewLayout layout, nint section)
+        {
+            return 4f;
+        }
+    }
+    public class DetailCell : UICollectionViewCell
+    {
+        private UIImageView imageView;
+        private UILabel titleView;
+
+        [Export("initWithFrame:")]
+        public DetailCell(CGRect frame) : base(frame)
+        {
+            BackgroundColor = UIColor.FromRGB(199, 21, 133);
+        }
+        public void SetImage(string uri,string title)
+        {
+            var colCount = 2f;
+            if (UIDevice.CurrentDevice.Model.ToLower().Contains("ipad")) colCount = 3f;
+            var width = UIScreen.MainScreen.Bounds.Width / colCount - 8;
+            var height = width / 16f * 9f;
+            var img = ScaledToSize(FromUrl(uri), new CGSize(width, height));
+            if (imageView == null)
+            {
+                imageView = new UIImageView(img);
+                ContentView.AddSubview(imageView);
+                titleView = new UILabel
+                {
+                    Text = title,
+                    TextColor = UIColor.White,
+                    BackgroundColor = UIColor.FromRGBA(0, 0, 0, 100),
+                    TextAlignment = UITextAlignment.Right
+                };
+                titleView.Frame = new CGRect(0d, height - 20d, width, 20d);
+                imageView.AddSubview(titleView);
+            }
+            else
+            {
+                imageView.Image = img;
+                titleView.Text = title;
+            }
+        }
+
+        private UIImage ScaledToSize(UIImage img, CGSize size)
+        {
+            if (img.Size == size)
+            {
+                return img;
+            }
+            UIGraphics.BeginImageContextWithOptions(size, false, 0f);
+            img.Draw(new CGRect(0f, 0f, size.Width, size.Height));
+            var simg = UIGraphics.GetImageFromCurrentImageContext();
+            UIGraphics.EndImageContext();
+            return simg;
+        }
+        private UIImage FromUrl(string uri)
+        {
+            using (var url = new NSUrl(uri))
+            using (var data = NSData.FromUrl(url))
+                return UIImage.LoadFromData(data);
+        }
+
+    }
+}
--- OneEchan.iOS/Controller/ViewController.cs
+++ OneEchan.iOS/Controller/ViewController.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Foundation;
-using MonoTouch.Dialog;
 using OneEchan.Core.Common.Api.Model;
-using OneEchan.iOS.DataSource;
+using OneEchan.iOS.Controller;
 using OneEchan.Shared;
 using UIKit;
 
 namespace OneEchan.iOS
 {
-	public partial class ViewController : UITableViewController
+    public partial class ViewController : UITableViewController
     {
+        private const string _mainListCellId = "MainListCell";
         private int _page = 0;
         private bool _hasMore = true;
         private bool _isLoading;
 
-        public List<AnimateListModel> List { get; private set; }
+        public List<AnimateListModel> List { get; private set; } = new List<AnimateListModel>();
         public ViewController (IntPtr handle) : base (handle)
         {
-            var source = new MainListDataSource(this);
-            source.LoadMore += (sender, e) => LoadMore();
-            TableView.Source = source;
-            List = new List<AnimateListModel>();
+            //var source = new MainListDataSource(this);
+            //source.LoadMore += (sender, e) => LoadMore();
+            //TableView.Source = source;
             RefreshControl = new UIRefreshControl();
             RefreshControl.ValueChanged += (sender, e) => Refresh();
         }
@@ -31,10 +29,10 @@ namespace OneEchan.iOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+            NavigationItem.BackBarButtonItem = new UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: null, action: null);
             NavigationController.NavigationBar.BarStyle = UIBarStyle.Black;
             NavigationController.NavigationBar.TintColor = UIColor.White;
             NavigationController.NavigationBar.BarTintColor = UIColor.FromRGB(199, 21, 133);
-            InvokeOnMainThread(() => RefreshControl.BeginRefreshing());
             Refresh();
         }
 
@@ -48,10 +46,15 @@ namespace OneEchan.iOS
         {
             if (_isLoading) return;
             _isLoading = true;
+            InvokeOnMainThread(() => RefreshControl.BeginRefreshing());
             _page = 0;
             var item = await Core.Common.Api.Home.GetList(_page++, LanguageHelper.PrefLang);
             if (!item.Success)
+            {
+                InvokeOnMainThread(() => RefreshControl.EndRefreshing());
+                _isLoading = false;
                 return;
+            }
             List = item.List.ToList();
             _hasMore = item.HasMore;
             TableView.ReloadData();
@@ -59,6 +62,53 @@ namespace OneEchan.iOS
             _isLoading = false;
         }
 
+        public override nint RowsInSection(UITableView tableView, nint section) => List.Count;
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell(_mainListCellId);
+            if (cell == null)
+                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, _mainListCellId);
+            var row = indexPath.Row;
+            cell.TextLabel.Text = List[row].Name;
+            cell.DetailTextLabel.Text = GetUpdate(List[row].LastUpdateTime);
+            cell.DetailTextLabel.TextColor = UIColor.Gray;
+            return cell;
+        }
+
+        public override void WillDisplay(UITableView tableView, UITableViewCell cell, NSIndexPath indexPath)
+        {
+            if (indexPath.Row == List.Count - 1)
+                LoadMore();
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            var detail = this.Storyboard.InstantiateViewController(nameof(DetailCollectionViewController)) as DetailCollectionViewController;
+            if (detail != null)
+            {
+                detail.Title = List[indexPath.Row].Name;
+                detail.ID = List[indexPath.Row].ID;
+                this.NavigationController.PushViewController(detail, true);
+            }
+        }
+
+        private string GetUpdate(TimeSpan time)
+        {
+            if (time.Days != 0)
+            {
+                return $"{time.Days} days ago";
+            }
+            if (time.Hours != 0)
+            {
+                return $"{time.Hours} hours ago";
+            }
+            if (time.Minutes != 0)
+            {
+                return $"{time.Minutes} minutes ago";
+            }
+            return "Just now";
+        }
         private async void LoadMore()
         {
             if (!_hasMore || _isLoading) return;
